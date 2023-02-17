0f60e5af6832d6ddfcfa15f0bafa4ac9ec29e6ba
Sakhile <fortunekhamz@gmail.com>
2023-02-17 05:06:52 -0800

    Task 12 commit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,15 +9,10 @@
 int main(void)
 {
 	int c = 0;
-
 	int f_d;
-
 	int l_d;
-
 	int c2;
-
 	int f_d2;
-
 	int l_d2;
 
 	while (c <= 98)
@@ -49,4 +44,4 @@ int main(void)
 	}
 	putchar('\n');
 	return (0);
-}			
+}
