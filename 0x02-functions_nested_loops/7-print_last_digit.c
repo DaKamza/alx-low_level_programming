a0dcd0c84153510a8d06911b9308e947eb340854
Sakhile <fortunekhamz@gmail.com>
2023-02-21 15:49:55 -0800

    Task 7 comit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * print_last_digit -prints last digit
+ * print_last_digit - prints last digit
  * @n: number to be checked
  * Return: value of the last digit
  */
 int print_last_digit(int n)
 {
-	int last = n % 10;
+	int last = n % 15;
 
 	if (last < 0)
 		last *= -1;
