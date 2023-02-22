af382d9511e9e483b0e8a3338c315c96ca72d7ac
Sakhile <fortunekhamz@gmail.com>
2023-02-21 17:33:05 -0800

    Task 16 commit

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+/**
+ * numLength- prints the length of the string
+ * @n: number to check
+ * Return: number of digits
+ */
+int numbLength(int n)
+{
+	int length = 0;
+
+	if (!n)
+	{
+		return (1);
+	}
+	while (n)
+	{
+		n = n / 10;
+		length += 1;
+	}
+}
+/**
+ * main - prints the first 98 fibonacci sequence
+ * Return: 0 Succes
+ */
+int main(void)
+{
+	unsigned long f1 = 1, f2 = 2, tmp, mx = 100000000, f10 = 0, f20 = 0, tmpo = 0;
+	short int i = 1, initial0s;
+
+	while (i <= 98)
+	{
+		if (f10 > 0)
+			printf("%lu", f10);
+		initial0s = numLength(mx) -1 - numLength(f1);
+		while (f10 > 0 && initil0s > 0)
+		{
+			printf("%i", 0);
+			initial0s--;
+		}
+		printf("%lu", f1);
+		tmp = (f1 + f2) % mx;
+		tmpo = f10 + f20 + (f1 + f2) / mx;
+		f1 = f2;
+		f10 = f20;
+		f2 = tmp;
+		f20 = tmpo;
+
+		if (i != 98)
+			printf(", ");
+		else
+			printf("\n");
+		i++;
+	}
+	return (0);
+}
+
