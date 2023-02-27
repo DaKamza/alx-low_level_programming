0bbab572233bd142101a8d957f5018d1f52b35c4
Sakhile <fortunekhamz@gmail.com>
2023-02-27 03:16:23 -0800

    Task 8 commit

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_array - prints the given array
+ * @a: array to print
+ * @n: number of values to print
+ */
+void print_array(int *a, int n)
+{
+	int t;
+
+	for (t = 0; t < n; t++)
+	{
+		printf("%d", a[t]);
+		if (t != n - 1)
+			printf(", ");
+	}
+	printf('\n');
+}
