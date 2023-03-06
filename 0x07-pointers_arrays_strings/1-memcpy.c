04925b9bec4d9991c40ce24c2c3e7010e5e35cd6
Sakhile <fortunekhamz@gmail.com>
2023-03-06 09:58:19 -0800

    Task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,12 +8,10 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i;
-	unsigned char *destination = dest;
-	unsigned char *source = src;
+	int i;
 
 	for (i = 0; i < n; i++)
-		destination[i] = source[i];
+		dest[i] = src[i];
 
 	return (dest);
 }
