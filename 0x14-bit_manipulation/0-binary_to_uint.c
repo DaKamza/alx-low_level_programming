1fc68d0f40c279045f16e0123152608371b2aaee
Sakhile <smkhamz@hotmail.com>
2023-04-08 03:28:04 -0700

    task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,16 +9,16 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int con_val = 0;
 
-	if (*b == NULL)
+	if (b == NULL)
 		return (0);
 
-	while (*b != NULL)
+	while (b != NULL)
 	{
-		if (*b < '0' || *b > '1')
+		if (b < '0' || b > '1')
 		{
 			return (0);
 		}
-		con_num = (con_num * 2) + (*b - '0');
+		con_num = (con_num * 2) + (b - '0');
 		b++;
 
 	}
