1ad7204ffddfa67e164b5e9f1177a735970ae9aa
Sakhile <smkhamz@hotmail.com>
2023-04-11 15:20:54 -0700

    Task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,35 +10,19 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	FILE *fd;
+	char *fb;
+	FILE *file;
+	ssize_t count;
 
-	char fb[letters];
-
-	size_t = count;
-
-	if (!filename)
+	file = fopen(filename, "r");
+	if (file < 0)
 		return (0);
 
-	fd = fopen(filename, "r");
-
-	if (fd == NULL)
-		return (0);
-
-	fb = maloc(sizeof(char) * letters);
-
-	if (fb == NULL)
-	{
-		fclose(fd);
-		return (0);
-	}
-
-
-	count = fread(fb, sizeof(char), letters, fd);
-
-	fwrite(fb, sizeof(char), count, stdout);
+	file = malloc(sizeof(char) * letters);
+	count = write(STDOUT_FILENO, fb, (read(file, fb, letters)));
 
 	free(fb);
-	fclose(fd);
+	close(file);
 
 	return (count);
 }
