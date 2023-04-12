1911f0dbf31b8f5ffc9abba18eb931572941524b
Sakhile <smkhamz@hotmail.com>
2023-04-11 17:54:17 -0700

    Task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,18 +9,21 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	FILE *file;
+	int file = 0;
+	int len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	file = fopen(filename, "w");
-	if (file == NULL)
+	file = fopen(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	write(file, text_content, len);
+
+	if (file < 0)
 		return (-1);
 
 	if (!text_content)
 	{
-		fprintf(file, "%s", text_content);
+		return (-1);
 	}
 
 	close(file);
