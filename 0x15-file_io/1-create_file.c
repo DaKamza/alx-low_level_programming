48e5db5d0c505dff758f57083272c520f4ed3174
Sakhile <smkhamz@hotmail.com>
2023-04-11 18:42:07 -0700

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,12 +24,12 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	if (text_content)
+	if (text_content != NULL)
 	{
 		while (text_content[len])
-		len++;
+			len++;
 
-		if (write(file, text_content, len) == -1)
+		if (write(file, text_content, len) != len)
 		{
 			close(file);
 			return (-1);
